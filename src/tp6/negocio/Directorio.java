eabb00564d54e052e321a3fa5c535b9d9cff2025
Escobar Jonathan <jony.escobar14@gmail.com>
2023-05-14 22:40:36 -0300

    Subiendo proyecto

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -1,4 +1,3 @@
-
 package tp6.negocio;
 
 import java.util.ArrayList;
@@ -6,40 +5,34 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import javax.swing.JOptionPane;
 
 // buscarTeléfono() que en base a un apellido nos devuelve una lista con los nros de 
 //teléfono asociados a dicho apellido.
 public class Directorio {
-    private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
-    
-    public Cliente buscarCliente(String numTelefono){
-        for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
-            String key = entry.getKey();
-            Cliente value = entry.getValue();
-            if(numTelefono.equals(key)){
-                return value;
+
+    private HashMap<String, Cliente> registro = new HashMap<String, Cliente>();
+
+    public Cliente buscarCliente(String numTelefono) {
+
+        return registro.get(numTelefono);
+    }
+
+    public ArrayList<Cliente> buscarClientes(String ciudad) {
+
+        ArrayList<Cliente> buscar = new ArrayList();
+        Set<String> claves = registro.keySet();
+        Iterator<String> it = claves.iterator();
+        while (it.hasNext()) {
+            String claveActual = it.next();
+            Cliente clienteActual = registro.get(claveActual);
+
+            if (clienteActual.getCiudad().equals(ciudad)) {
+                buscar.add(clienteActual);
             }
         }
-        System.out.println("No se encontrò el telefono asociado a un cliente");
-        return null;
-    }
-public ArrayList<Cliente> buscarClientes(String ciudad){
-        
-        ArrayList<Cliente>buscar= new ArrayList();
-        Set<String> claves=registro.keySet();
-       Iterator<String> it = claves.iterator();
-       while(it.hasNext()){
-           String claveActual=it.next();
-           Cliente clienteActual=registro.get(claveActual);
-           
-           if(clienteActual.getCiudad().equals(ciudad)){
-               buscar.add(clienteActual);
-           }         
-       }
         return buscar;
     }
-
-
 
 //AgregarCliente() que permite registrar un nuevo cliente con su respectivo nro de 
 //teléfono. Siendo el nro del teléfono la clave del mismo.
@@ -50,12 +43,12 @@ public ArrayList<Cliente> buscarClientes(String ciudad){
             agregado = true;
         }
         return agregado;
-    }   
-    
-    public ArrayList<String> buscarTelefono(String apellido){
-        ArrayList <String> num= new ArrayList<String>();
+    }
+
+    public ArrayList<String> buscarTelefono(String apellido) {
+        ArrayList<String> num = new ArrayList<String>();
         for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
-            if(registro.containsValue(apellido)){
+            if (registro.containsValue(apellido)) {
                 num.add(entry.getKey());
             }
         }
@@ -66,13 +59,14 @@ public ArrayList<Cliente> buscarClientes(String ciudad){
 //        }
         return num;
     }
-    
-    public void borrarCliente(String telefono){
-        Cliente aux = registro.remove(telefono);
-        if (aux!=null) {
-            System.out.println("Se ha eliminado correctamente.");
-        } else {
-            System.out.println("Cliente no encontrado.");
+
+    public boolean borrarCliente(String telefono) {
+        //Cliente aux = 
+        boolean borrado = false;
+        if (registro.containsKey(telefono)) {
+            registro.remove(telefono);
+            borrado = true;
         }
+        return borrado;
     }
 }
